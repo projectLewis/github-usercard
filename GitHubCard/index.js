29bf041e1e092b3caa2bb62e43e32c9fdb037299
Seth Lewis <lewiscatenary@gmail.com>
2019-07-11 10:54:23 -0500

    added axios, requested my githhub profile

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// const axios = require('axios');
+
+axios.get('https://api.github.com/users/projectlewis')
+  .then((response) => {
+    // const gitRes = JSON.parse(response);
+    const {avatar_url, name, login, location, html_url, followers, following} = response.data;
+    console.log(avatar_url);
+  }).catch((error) => {
+    console.log(error);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
